5df30249664c7a8ab6b37517b7d6fb2b8b6a0aab
naleilan <leilan.n@gmail.com>
2025-04-15 12:25:21 +0330

    add pizza-menu some jsx

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -1,35 +1,90 @@
 import React, { useEffect, useState } from 'react';
 
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
+
 export default function App() {
-  const [advice, setAdvice] = useState('');
-  const [count, setCount] = useState(0);
-
-  async function getAdvice() {
-    const res = await fetch(
-      `https://api.adviceslip.com/advice?timestamp=${Date.now()}`
-    );
-    const data = await res.json();
-    setAdvice(data.slip.advice);
-    setCount(c => c + 1);
-  }
-
-  useEffect(function () {
-    getAdvice();
-  }, []);
-
   return (
     <div>
-      <h3>{advice}</h3>
-      <button onClick={getAdvice}>get Advice</button>
-      <Message count={count} />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Message(props) {
+function Header() {
   return (
-    <p>
-      you have read <strong> {props.count} </strong> piese of Advice.
-    </p>
+    <div>
+      <h1>Fast React Pizza.co</h1>
+    </div>
   );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="./public/pizzas/focaccia.jpg" alt="focaccia" />
+      <h2>Pizza focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()}. We're open.</footer>;
 }
