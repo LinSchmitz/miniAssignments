a1eeacf239749298780c1fd6be858707128234aa
naleilan <leilan.n@gmail.com>
2025-04-17 21:24:56 +0330

    add conditional renderin on pizza list

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -65,15 +65,24 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const pizzasNumber = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-
-      <ul className="pizzas">
-        {pizzaData.map(pizza => (
-          <Pizza pizzaObject={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {/* this conditinal rendering  
+       is because when pizzas.length = 0 
+       we dont want to render ul for nothing
+       rendering happens just when we have data in list
+    */}
+      {pizzasNumber > 0 && (
+        <ul className="pizzas">
+          {pizzas.map(pizza => (
+            <Pizza pizzaObject={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
