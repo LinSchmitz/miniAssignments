a1f86c6bfbadf5d4bc1ac87122b72af4c424e56a
naleilan <leilan.n@gmail.com>
2025-04-18 09:37:20 +0330

    add order component

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -114,17 +114,23 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We are Open until {closeHour}:00. Come visit us or order ONLINE.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We are Open until {props.closeHour}:00. Come visit us or order ONLINE.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
