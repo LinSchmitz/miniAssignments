19e2934acd6bc746f75fd8ad57fac7459f9b645e
naleilan <leilan.n@gmail.com>
2025-04-18 13:08:06 +0330

    add some css to steps app

--- Steps/src/App.jsx
+++ Steps/src/App.jsx
@@ -2,5 +2,34 @@ import React from 'react';
 import './index.css';
 
 export default function App() {
-  return <div>steps</div>;
+  return (
+    <div className="card">
+      <Numbers />
+      <Main />
+      <Buttons />
+    </div>
+  );
+}
+
+function Numbers() {
+  return (
+    <div className="numbers">
+      <div className="number">1</div>
+      <div className="number">2</div>
+      <div className="number">3</div>
+    </div>
+  );
+}
+
+function Main() {
+  return <main>Step</main>;
+}
+
+function Buttons() {
+  return (
+    <div>
+      <button>Previous</button>
+      <button>Next</button>
+    </div>
+  );
 }
