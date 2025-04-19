71f292c4da077d9b26f3a5d954e98575bd9d9cf4
naleilan <leilan.n@gmail.com>
2025-04-19 14:54:53 +0330

    change logic

--- Steps/src/App.jsx
+++ Steps/src/App.jsx
@@ -1,58 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 const msg = ['Learn javaScript', 'Learn Reactjs', 'Code alot'];
 
 export default function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    alert('prev');
+  }
+
+  function handleNext() {
+    alert('Next');
+  }
+
   return (
     <div className="card">
-      <Numbers />
-      <Buttons />
-    </div>
-  );
-}
+      <div className="numbers">
+        <div className="number">
+          <div className={step >= 1 ? 'active' : ''}>1</div>
+          <div className={step >= 2 ? 'active' : ''}>2</div>
+          <div className={step >= 3 ? 'active' : ''}>3</div>
+        </div>
 
-function Numbers() {
-  const step = 1;
-  return (
-    <div className="numbers">
-      <div className="number">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
+        <main className="main">
+          <p>
+            Steps {step}: {msg[step - 1]}{' '}
+          </p>
+        </main>
       </div>
-      <Main step={step} />
-    </div>
-  );
-}
 
-function Main({ step }) {
-  return (
-    <main className="main">
-      <p>
-        Steps {step}: {msg[step - 1]}{' '}
-      </p>
-    </main>
-  );
-}
-
-function handlePrevious() {
-  alert('prev');
-}
-
-function handleNext() {
-  alert('Next');
-}
-
-function Buttons() {
-  return (
-    <div className="btns">
-      <button className="btn" onClick={handlePrevious}>
-        Previous
-      </button>
-      <button className="btn" onClick={handleNext}>
-        Next
-      </button>
+      <div className="btns">
+        <button className="btn" onClick={handlePrevious}>
+          Previous
+        </button>
+        <button className="btn" onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
