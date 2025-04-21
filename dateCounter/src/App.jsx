1ad7f6f7da48de36b652ac8ac7b80d693cf0deb9
naleilan <leilan.n@gmail.com>
2025-04-21 19:55:33 +0330

    added elements and css

--- dateCounter/src/App.jsx
+++ dateCounter/src/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default function App() {
-  return <div>date counter</div>;
+  return (
+    <div className="card">
+      <div className="rows">
+        <button>-</button>
+        <h3>Step:1 </h3>
+        <button>+</button>
+      </div>
+      <div className="rows">
+        <button>-</button>
+        <h3>Count:1 </h3>
+        <button>+</button>
+      </div>
+      <p>msg</p>
+    </div>
+  );
 }
