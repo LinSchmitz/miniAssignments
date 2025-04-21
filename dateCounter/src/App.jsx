d812e5053c3587fdd2c402d17ab92110afe987b3
naleilan <leilan.n@gmail.com>
2025-04-21 20:08:09 +0330

    added handles

--- dateCounter/src/App.jsx
+++ dateCounter/src/App.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function App() {
+  const dt = new Date().toLocaleDateString();
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+
+  function handlePreStep() {
+    setStep(step => step - 1);
+  }
+
+  function handleNextStep() {
+    setStep(step => step + 1);
+  }
+
+  function handlePreCount() {
+    setCount(count => count - 1);
+    dt == dt + count;
+  }
+
+  function handleNextCount() {
+    setCount(count => count + 1);
+  }
+
   return (
     <div className="card">
       <div className="rows">
-        <button>-</button>
-        <h3>Step:1 </h3>
-        <button>+</button>
+        <button onClick={handlePreStep}>-</button>
+        <h3>Step:{step} </h3>
+        <button onClick={handleNextStep}>+</button>
       </div>
       <div className="rows">
-        <button>-</button>
-        <h3>Count:1 </h3>
-        <button>+</button>
+        <button onClick={handlePreCount}>-</button>
+        <h3>Count:{count} </h3>
+        <button onClick={handleNextCount}>+</button>
       </div>
-      <p>msg</p>
+      <p>
+        {count} Days from today is {dt}
+      </p>
     </div>
   );
 }
