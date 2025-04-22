336c34518a64af53ffa3d4855fff2eefa76215bb
naleilan <leilan.n@gmail.com>
2025-04-22 09:29:10 +0330

    added 4 main components

--- travelList/src/App.jsx
+++ travelList/src/App.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
 export default function App() {
-  return <div>travel list</div>;
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do You need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already packed X(X%)</em>
+    </footer>
+  );
 }
