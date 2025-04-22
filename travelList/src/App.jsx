ae06e2cbfd8f1ce8e8c2b6b66a0597d46e85e92c
naleilan <leilan.n@gmail.com>
2025-04-22 09:47:45 +0330

    added some css

--- travelList/src/App.jsx
+++ travelList/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -22,12 +27,14 @@ function Form() {
   );
 }
 function PackingList() {
+  console.log(initialItems);
+
   return <div className="list">LIST</div>;
 }
 function Stats() {
   return (
-    <footer>
-      <em>💼 You have X items on your list, and you already packed X(X%)</em>
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
 }
