cd0ee9a374643a76f15367bebd55bb91ad821774
naleilan <leilan.n@gmail.com>
2025-04-24 13:47:09 +0330

    created smilly emoji

--- moodTracker/src/App.jsx
+++ moodTracker/src/App.jsx
@@ -1,5 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './index.css';
 
 export default function App() {
-  return <div>Mood tracker</div>;
+  return (
+    <div>
+      Mood tracker
+      <SmillyEmoji />
+    </div>
+  );
+}
+
+function SmillyEmoji() {
+  return (
+    <div className="container">
+      <div className="emoji">
+        <div className="smilly">
+          <div className="eye left"></div>
+          <div className="eye right"></div>
+          <div className="cheek left"></div>
+          <div className="cheek right"></div>
+          <div className="mouth"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
