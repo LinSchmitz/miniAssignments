b169d8d8fabe6dd29763f91a4d01aa091a44bbab
naleilan <leilan.n@gmail.com>
2025-04-24 14:55:30 +0330

    created all emojies

--- moodTracker/src/App.jsx
+++ moodTracker/src/App.jsx
@@ -5,22 +5,39 @@ export default function App() {
   return (
     <div>
       Mood tracker
-      <SmillyEmoji />
+      <EmojiRow />
+      <div className="half-circle"></div>
     </div>
   );
 }
 
-function SmillyEmoji() {
+function EmojiRow() {
   return (
     <div className="container">
-      <div className="emoji">
-        <div className="smilly">
-          <div className="eye left"></div>
-          <div className="eye right"></div>
-          <div className="cheek left"></div>
-          <div className="cheek right"></div>
-          <div className="mouth"></div>
-        </div>
+      <div className="emoji-row">
+        <SmillyEmoji mood="happy" />
+        <SmillyEmoji mood="sad" />
+        <SmillyEmoji mood="surprised" />
+        <SmillyEmoji mood="angry" />
+        <SmillyEmoji mood="winking" />
+      </div>
+    </div>
+  );
+}
+
+function SmillyEmoji({ mood }) {
+  return (
+    <div className={`emoji ${mood}`}>
+      <div className="smiley">
+        <div
+          className={`eye left ${mood === 'winking' ? 'winking' : ''}`}
+        ></div>
+        <div
+          className={`eye right ${mood === 'winking' ? 'winking' : ''}`}
+        ></div>
+        <div className={`cheek left ${mood}`}></div>
+        <div className={`cheek right ${mood}`}></div>
+        <div className={`mouth ${mood}`}></div>
       </div>
     </div>
   );
