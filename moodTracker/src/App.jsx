98e95e55df157588b4ef3a9f7d268f5cb30dbae7
naleilan <leilan.n@gmail.com>
2025-04-24 15:50:30 +0330

    added logic

--- moodTracker/src/App.jsx
+++ moodTracker/src/App.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 export default function App() {
+  const [selectedMood, setSelectedMood] = useState(null);
+
   return (
     <div className="app">
       <h3>Mood tracker</h3>
-      <EmojiRow />
-      <div className="half-circle"></div>
+      <EmojiRow onSelectMood={setSelectedMood} />
+      <Main selectedMood={selectedMood} />
     </div>
   );
 }
 
-function EmojiRow() {
+function EmojiRow({ onSelectMood }) {
   return (
     <div className="container">
       <div className="emoji-row">
-        <SmillyEmoji mood="happy" title="happy" />
-        <SmillyEmoji mood="good" title="good" />
-        <SmillyEmoji mood="surprised" title="surprised" />
-        <SmillyEmoji mood="sad" title="sad" />
-        <SmillyEmoji mood="supersad" title="super sad" />
-        <SmillyEmoji mood="angry" title="angry" />
+        <SmillyEmoji mood="happy" title="happy" onClick={onSelectMood} />
+        <SmillyEmoji mood="good" title="good" onClick={onSelectMood} />
+        <SmillyEmoji
+          mood="surprised"
+          title="surprised"
+          onClick={onSelectMood}
+        />
+        <SmillyEmoji mood="sad" title="sad" onClick={onSelectMood} />
+        <SmillyEmoji mood="supersad" title="super sad" onClick={onSelectMood} />
+        <SmillyEmoji mood="angry" title="angry" onClick={onSelectMood} />
       </div>
     </div>
   );
 }
 
-function SmillyEmoji({ mood, title }) {
+function SmillyEmoji({ mood, title, onClick }) {
   return (
-    <div className={`emoji ${mood}`} title={title}>
+    <div
+      className={`emoji ${mood}`}
+      title={title}
+      onClick={() => onClick({ mood, title })}
+    >
       <div className="smiley">
         <div
           className={`eye left ${mood === 'supersad' ? 'supersad' : ''}`}
@@ -36,9 +46,22 @@ function SmillyEmoji({ mood, title }) {
         <div
           className={`eye right ${mood === 'supersad' ? 'supersad' : ''}`}
         ></div>
-
         <div className={`mouth ${mood}`}></div>
       </div>
     </div>
+  );
+}
+
+function Main({ selectedMood }) {
+  if (!selectedMood) return <main>Select a mood</main>;
+
+  return (
+    <main>
+      <h4>Current Mood:</h4>
+      <div style={{ marginTop: '1rem' }}>
+        <SmillyEmoji mood={selectedMood.mood} title={selectedMood.title} />
+        <p>{selectedMood.title}</p>
+      </div>
+    </main>
   );
 }
