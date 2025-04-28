79c197fb1823549c607fbdc11534c1ef487bd9e6
naleilan <leilan.n@gmail.com>
2025-04-28 12:41:59 +0330

    added split bill template

--- splitBill/src/components/App.jsx
+++ splitBill/src/components/App.jsx
@@ -1,57 +1,55 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
   return (
     <div>
-      <Bill />
-      <Percentage />
-      <Service />
-      <Receipt />
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage />
+      <SelectPercentage />
+      <Output />
       <Reset />
     </div>
   );
 }
 
-function Bill() {
+function BillInput() {
   return (
     <div>
       <h4>How much was the bill? </h4>
-      <input type="text" />
+      <input type="text" placeholder="Bill value" />
     </div>
   );
 }
 
-function Percentage() {
+function SelectPercentage() {
   return (
     <div>
       <h4>How did you like the Service? </h4>
-      <select name="good" id="">
-        <option value="ok">It was ok(5%)</option>
-        <option value="good">It was good(10%)</option>
-        <option value="great">It was great(20%)</option>
+      <select>
+        <option value="0">Dissatisfied(0%)</option>
+        <option value="5">It was ok(5%)</option>
+        <option value="10">It was good(10%)</option>
+        <option value="20">It was great(20%)</option>
       </select>
     </div>
   );
 }
 
-function Service() {
+function Output() {
   return (
     <div>
-      <h4>How did your friend like the Service? </h4>
-      <select name="good" id="">
-        <option value="ok">It was ok(5%)</option>
-        <option value="good">It was good(10%)</option>
-        <option value="great">It was great(20%)</option>
-      </select>
-    </div>
-  );
-}
-
-function Receipt() {
-  return (
-    <div>
-      <h4>You pay ${92}($80+$12 tip) </h4>
+      <h4>
+        You pay ${0}(${0} + $12 tip)
+      </h4>
     </div>
   );
 }
