8984db74e94dbffb487c86ba67fe1318bdf173a8
naleilan <leilan.n@gmail.com>
2025-04-29 08:50:36 +0330

    finished friend component logic

--- split-The-Bills/src/components/App.jsx
+++ split-The-Bills/src/components/App.jsx
@@ -38,21 +38,32 @@ function FriendsList() {
   return (
     <ul>
       {friends.map(friend => (
-        <Friend
-          name={friend.name}
-          balance={friend.balance}
-          id={friend.id}
-          key={friends.id}
-        />
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
   );
 }
 
-function Friend({ id, name, balance }) {
+function Friend({ friend }) {
   return (
-    <li key={id}>
-      {name} {balance}
+    <li key={friend.id}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} €{Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you €{Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even </p>}
+      <button className="button">Select</button>
     </li>
   );
 }
