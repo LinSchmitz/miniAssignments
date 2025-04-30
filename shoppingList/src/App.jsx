505d65c1f85fdfa278644bf68321690dad2ea709
naleilan <leilan.n@gmail.com>
2025-04-30 17:12:08 +0330

    added some css

--- shoppingList/src/App.jsx
+++ shoppingList/src/App.jsx
@@ -1,21 +1,46 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-const initialData = [
+const productsList = [
   {
-    id: 118836,
-    name: 'Chair',
-    image: 'https://i.pravatar.cc/48?u=118836',
+    id: 1,
+    name: 'MacBook Pro 16” M2',
+    price: 2799,
+    image: 'https://picsum.photos/id/1/300',
+    description:
+      "Apple's high-performance laptop with M2 Pro chip, 16GB RAM, and 512GB SSD.",
   },
   {
-    id: 933372,
-    name: 'Table',
-    image: 'https://i.pravatar.cc/48?u=933372',
+    id: 2,
+    name: 'Dell XPS 13',
+    price: 1499,
+    image: 'https://picsum.photos/id/2/300',
+    description:
+      'Sleek Windows ultrabook with 12th Gen Intel i7, 16GB RAM, and 512GB SSD.',
   },
   {
-    id: 499476,
-    name: 'Sofa',
-    image: 'https://i.pravatar.cc/48?u=499476',
+    id: 3,
+    name: 'iPhone 15 Pro',
+    price: 1099,
+    image: 'https://picsum.photos/id/6/300',
+    description:
+      'Apple’s latest iPhone with A17 chip, 128GB storage, and titanium frame.',
+  },
+  {
+    id: 4,
+    name: 'Samsung Galaxy S24 Ultra',
+    price: 1199,
+    image: 'https://picsum.photos/id/6/300',
+    description:
+      'Flagship Android phone with Snapdragon 8 Gen 3, 200MP camera, and 5000mAh battery.',
+  },
+  {
+    id: 5,
+    name: 'Lenovo Legion 5 Pro',
+    price: 1599,
+    image: 'https://picsum.photos/id/6/300',
+    description:
+      'Gaming laptop with AMD Ryzen 7, RTX 4070, 16GB RAM, and 1TB SSD.',
   },
 ];
 
@@ -32,7 +57,7 @@ export default function App() {
 function Products() {
   return (
     <ul>
-      {initialData.map(item => (
+      {productsList.map(item => (
         <ProductsList item={item} key={item.id} />
       ))}
     </ul>
@@ -40,7 +65,16 @@ function Products() {
 }
 
 function ProductsList({ item }) {
-  return <li>{item.name}</li>;
+  return (
+    <li>
+      <img src={item.image} />
+      <div className="items">
+        <span>{item.name}</span>
+        <span>{item.description}</span>
+        <span>€{item.price}</span>
+      </div>
+    </li>
+  );
 }
 
 function AddItems() {
