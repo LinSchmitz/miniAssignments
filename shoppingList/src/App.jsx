5412abe13a2b45370eaee83fe09b37fb240c3660
naleilan <leilan.n@gmail.com>
2025-05-02 09:21:07 +0330

    added media query

--- shoppingList/src/App.jsx
+++ shoppingList/src/App.jsx
@@ -6,35 +6,35 @@ const initialItems = [
     id: 1,
     name: 'MacBook Pro 16” M2',
     price: 2799,
-    image: 'https://picsum.photos/id/1/300',
+    image: 'https://picsum.photos/id/1/300/200',
     description: ' M2 Pro chip, 16GB RAM, and 512GB SSD.',
   },
   {
     id: 2,
     name: 'Dell XPS 13',
     price: 1499,
-    image: 'https://picsum.photos/id/2/300',
+    image: 'https://picsum.photos/id/2/300/200',
     description: ' 12th Gen Intel i7, 16GB RAM, and 512GB SSD.',
   },
   {
     id: 3,
     name: 'iPhone 15 Pro',
     price: 1099,
-    image: 'https://picsum.photos/id/6/300',
+    image: 'https://picsum.photos/id/6/300/200',
     description: ' A17 chip, 128GB storage, and titanium frame.',
   },
   {
     id: 4,
     name: 'Samsung Galaxy S24 Ultra',
     price: 1199,
-    image: 'https://picsum.photos/id/6/300',
+    image: 'https://picsum.photos/id/6/300/200',
     description: ' Snapdragon 8 Gen 3, 200MP camera, and 5000mAh battery.',
   },
   {
     id: 5,
     name: 'Lenovo Legion 5 Pro',
     price: 1599,
-    image: 'https://picsum.photos/id/6/300',
+    image: 'https://picsum.photos/id/6/300/200',
     description: ' AMD Ryzen 7, RTX 4070, 16GB RAM, and 1TB SSD.',
   },
 ];
@@ -86,7 +86,7 @@ function Item({ item, quantities, onQuantities, onRemoveItems }) {
       <img src={item.image} alt={item.name} />
       <div className="items">
         <h3>{item.name}</h3>
-        <span>{item.description}</span>
+        <h4>{item.description}</h4>
 
         <div className="price-and-button">
           €{item.price}
@@ -150,7 +150,7 @@ function FormBill() {
           <Button className="button" width="30px">
             +
           </Button>
-          <Button className="button" width="30px">
+          <Button className="remove" width="30px">
             &times;
           </Button>
         </div>
@@ -160,14 +160,14 @@ function FormBill() {
         <h2>Total: €X</h2>
       </div>
 
-      <div class="buy-button-wrapper">
-        <Button className="button">BUY</Button>
+      <div className="buy-button-wrapper">
+        <Button className="btn-buy">BUY</Button>
       </div>
     </form>
   );
 }
 
-function Button({ children, onClick, width, color, className }) {
+function Button({ children, onClick, width, className }) {
   // const buttonClass = `${className} ${color ? 'button' : 'hover'}`;
 
   return (
