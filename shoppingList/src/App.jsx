7faa3f87403b01d522b5d374521fbf56236d5676
naleilan <leilan.n@gmail.com>
2025-05-07 08:21:39 +0330

    added shopping list

--- shoppingList/src/App.jsx
+++ shoppingList/src/App.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 
 const initialItems = [
@@ -20,191 +19,191 @@ const initialItems = [
     id: 3,
     name: 'iPhone 15 Pro',
     price: 1099,
-    image: 'https://picsum.photos/id/6/300/200',
+    image: 'https://picsum.photos/id/3/300/200',
     description: ' A17 chip, 128GB storage, and titanium frame.',
   },
   {
     id: 4,
     name: 'Samsung Galaxy S24 Ultra',
     price: 1199,
-    image: 'https://picsum.photos/id/6/300/200',
+    image: 'https://picsum.photos/id/4/300/200',
     description: ' Snapdragon 8 Gen 3, 200MP camera, and 5000mAh battery.',
   },
   {
     id: 5,
     name: 'Lenovo Legion 5 Pro',
     price: 1599,
-    image: 'https://picsum.photos/id/6/300/200',
+    image: 'https://picsum.photos/id/5/300/200',
     description: ' AMD Ryzen 7, RTX 4070, 16GB RAM, and 1TB SSD.',
   },
 ];
 
-export default function App() {
-  const [quantities, setQuantities] = useState({});
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
 
-  function handleRemoveItems(id) {
-    console.log('Remove item with ID:', id);
-    // setQuantities(false);
-    setQuantities(prev => {
-      const updated = { ...prev };
-      delete updated[id];
-      return updated;
-    });
+export default function App() {
+  const [items, setItems] = useState(initialItems);
+  const [showAddItem, setShowAddItem] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  function handleShowAddItem() {
+    setShowAddItem(show => !show);
+  }
+
+  function handleAddItem(item) {
+    setItems(items => [...items, item]);
+    setShowAddItem(false);
+  }
+
+  function handleSelection(item) {
+    // setSelectedItem(item);
+    setSelectedItem(cur => (cur?.id === item.id ? null : item));
+    setShowAddItem(false);
   }
 
   return (
     <div className="app">
-      <ItemsList
-        quantities={quantities}
-        onQuantities={setQuantities}
-        onRemoveItems={handleRemoveItems}
-      />
-      <FormBill />
+      <div className="sidebar">
+        <ItemList
+          items={items}
+          selectedItem={selectedItem}
+          onSelection={handleSelection}
+        />
+
+        {showAddItem && <FormAddItem onAddItem={handleAddItem} />}
+        <Button onClick={handleShowAddItem}>
+          {showAddItem ? 'Close' : 'Add Item'}
+        </Button>
+      </div>
+      {selectedItem && <FormBill selectedItem={selectedItem} />}
     </div>
   );
 }
 
-function ItemsList({ quantities, onQuantities, onRemoveItems }) {
-  const items = initialItems;
+function ItemList({ items, onSelection, selectedItem }) {
   return (
     <ul>
       {items.map(item => (
         <Item
           item={item}
           key={item.id}
-          quantities={quantities}
-          onQuantities={onQuantities}
-          onRemoveItems={onRemoveItems}
+          selectedItem={selectedItem}
+          onSelection={onSelection}
         />
       ))}
     </ul>
   );
 }
 
-function Item({ item, quantities, onQuantities, onRemoveItems }) {
-  function handleAdd(id) {
-    onQuantities(!quantities);
-    onQuantities(prev => ({
-      ...prev,
-      [id]: !prev[id],
-    }));
-  }
+function Item({ item, onSelection, selectedItem }) {
+  const isSelected = selectedItem?.id === item.id;
 
   return (
-    <li>
+    <li className={isSelected ? 'selected' : ''}>
       <img src={item.image} alt={item.name} />
-      <div className="items">
-        <h3>{item.name}</h3>
-        <h4>{item.description}</h4>
-
-        <div className="price-and-button">
-          €{item.price}
-          <Button
-            className={quantities[item.id] ? 'button empty' : 'button'}
-            onClick={() => handleAdd(item.id)}
-          >
-            {quantities[item.id] ? '' : 'Add to cart'}
-          </Button>
-        </div>
-        {quantities[item.id] && (
-          <div>
-            <AddItems onRemoveItems={onRemoveItems} item={item} />
-          </div>
-        )}
-      </div>
+      {item.name}
+      {item.price}
+      {item.description}
+      <Button onClick={() => onSelection(item)}>
+        {isSelected ? 'close' : 'Add to cart'}
+      </Button>
     </li>
   );
 }
 
-function AddItems({ item, onRemoveItems }) {
-  const [itemCount, setItemCount] = useState(0);
+function FormAddItem({ onAddItem }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://picsum.photos/id');
 
-  function handleAddItem() {
-    setItemCount(itemCount => itemCount + 1);
-    <FormBill itemCount={itemCount} />;
-  }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  function handleSubtractItem() {
-    setItemCount(itemCount => (itemCount > 0 ? itemCount - 1 : 0));
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newItem = {
+      id,
+      name,
+      price: 0,
+      image: `${image}?=${id}/300/200`,
+      description: '',
+    };
+    onAddItem(newItem);
+
+    setName('');
+    setImage('https://picsum.photos/id');
   }
 
   return (
-    <div className="button-group">
-      <Button
-        className="button"
-        onClick={() => handleSubtractItem}
-        width="30px"
-      >
-        -
-      </Button>
-      <h3>{itemCount} </h3>
-      <Button className="button" onClick={() => handleAddItem} width="30px">
-        +
-      </Button>
-      <Button
-        className="remove"
-        color={'var(--color-red)'}
-        onClick={() => onRemoveItems(item.id)}
-      >
-        Remove
-      </Button>
-    </div>
-  );
-}
+    <form className="form-add-item" onSubmit={handleSubmit}>
+      <label>💻 Item name</label>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
 
-function FormBill({ item, itemCount }) {
-  return (
-    <form className="form-bill">
-      <h2>Cart</h2>
-
-      <div className="cart-item">
-        <div className="item-left">
-          <img src="https://picsum.photos/id/1/50" alt="item" />
-          <div>
-            <h3>{item.id} </h3>
-            <h4>€Price</h4>
-          </div>
-        </div>
-
-        <div>
-          <Button className="button" width="30px">
-            -
-          </Button>
-          <span>{itemCount}</span>
-          <Button className="button" width="30px">
-            +
-          </Button>
-          <Button className="remove" width="30px">
-            &times;
-          </Button>
-        </div>
-      </div>
-
-      <div className="total">
-        <h2>Total: €X</h2>
-      </div>
-
-      <div className="buy-button-wrapper">
-        <Button className="btn-buy">BUY</Button>
-      </div>
+      <label>➕ Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={e => setImage(e.target.value)}
+      />
+      <Button>Add</Button>
     </form>
   );
 }
 
-function Button({ children, onClick, width, className }) {
-  // const buttonClass = `${className} ${color ? 'button' : 'hover'}`;
+function FormBill({ selectedItem }) {
+  const [num, setNum] = useState(1);
+  const [isShow, setIsShow] = useState(true);
+
+  const total = selectedItem.price * num;
+
+  function handleNumPlus() {
+    setNum(n => n + 1);
+  }
+
+  function handleNumMinues() {
+    if (num <= 0) return;
+    setNum(n => n - 1);
+  }
+
+  function handleRemove() {
+    setNum(0);
+    setIsShow(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
-    <button
-      className={`button ${className || ''}`}
-      // className={buttonClass}
-      onClick={onClick}
-      style={{
-        width: width || '90px',
-        // backgroundColor: color || 'var(--color-light1)',
-      }}
-    >
-      {children}
-    </button>
+    isShow && (
+      <form className="form-bill" onSubmit={handleSubmit}>
+        <h2>Cart</h2>
+
+        <div className="cart-row">
+          <div className="cart-info">
+            <img src="https://picsum.photos/id/6/50" alt="" />
+            <div>
+              <h3>{selectedItem.name} </h3>
+              <h3>${selectedItem.price}</h3>
+            </div>
+          </div>
+
+          <div className="cart-actions">
+            <Button onClick={handleNumMinues}>-</Button>
+            <h3>{num}</h3>
+            <Button onClick={handleNumPlus}>+</Button>
+            <Button onClick={handleRemove}>&times;</Button>
+          </div>
+        </div>
+
+        <h3 className="total">Total: ${Number(total)} </h3>
+
+        <Button className="button buy-button">BUY</Button>
+      </form>
+    )
   );
 }
