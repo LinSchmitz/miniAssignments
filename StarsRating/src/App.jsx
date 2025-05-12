7bc73d476a84983c47a2a1ca2f07e833c6171040
naleilan <leilan.n@gmail.com>
2025-05-12 17:53:23 +0330

    added starrating

---
+++ StarsRating/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const msg = ['Learn javaScript', 'Learn Reactjs', 'Code alot'];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep(s => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(s => s + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(is => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="card">
+          <div className="numbers">
+            <div className="number">
+              <div className={step >= 1 ? 'active' : ''}>1</div>
+              <div className={step >= 2 ? 'active' : ''}>2</div>
+              <div className={step >= 3 ? 'active' : ''}>3</div>
+            </div>
+
+            <main className="main">
+              <p>
+                Steps {step}: {msg[step - 1]}{' '}
+              </p>
+            </main>
+          </div>
+
+          <div className="btns">
+            <button className="btn" onClick={handlePrevious}>
+              Previous
+            </button>
+            <button className="btn" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
