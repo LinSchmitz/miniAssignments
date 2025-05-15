b6514f861ed14fc6042b03a4e72e831d04afb05c
naleilan <leilan.n@gmail.com>
2025-05-15 10:15:09 +0330

    added style

--- StarsRating/src/StarRating.jsx
+++ StarsRating/src/StarRating.jsx
@@ -1,10 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+};
+
+const starContainetStyle = {
+  display: 'flex',
+  gap: '4px',
+};
+
 export default function StarRating() {
   return (
-    <div>
-      <div>
+    <div style={containerStyle}>
+      <div style={starContainetStyle}>
         {Array.from({ length: 5 }, (_, i) => (
           <span>S{i + 1} </span>
         ))}
