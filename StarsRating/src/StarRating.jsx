f131c8af2a09a83caa432a792afc4109146e7e91
naleilan <leilan.n@gmail.com>
2025-05-16 08:23:52 +0330

    added readme

--- StarsRating/src/StarRating.jsx
+++ StarsRating/src/StarRating.jsx
@@ -18,6 +18,7 @@ export default function StarRating({
   className = '',
   message = [],
   defaultRating = 0,
+  onSetRating,
 }) {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
@@ -29,6 +30,11 @@ export default function StarRating({
     fontSize: `${size / 1.5}`,
   };
 
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
+
   return (
     <div style={containerStyle} className={className}>
       <div style={starContainetStyle}>
@@ -36,7 +42,7 @@ export default function StarRating({
           <Star
             key={i}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            onRate={() => setRating(i + 1)}
+            onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             color={color}
--- StarsRating/src/index.jsx
+++ StarsRating/src/index.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="Orange" maxRating={10} onSetRating={setMovieRating} />
+      <p>This Movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +24,7 @@ root.render(
     <StarRating color="red" size={24} />
     <StarRating color="blue" defaultRating={3} />
     <StarRating maxRating={10} />
+    <Test />
     {/* <StarRating /> */}
   </React.StrictMode>
 );
