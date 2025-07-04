cc26570598ed2b354a9046966d08244de18a8f3f
naleilan <leilan.n@gmail.com>
2025-07-04 06:57:37 +0330

    added darkMode

---
+++ Apps/darkMode/README.md
@@ -0,0 +1,25 @@
+# 🗓️ React Date Counter App
+
+A simple and interactive React app that helps you explore `useState`, perform dynamic updates, and manipulate dates based on user input. Great for learning how state updates can drive UI changes in real time.
+
+### 📸 Preview
+
+![alt text](public/img/dateCounter.png)
+
+## ✨ Features
+
+- 🔁 Adjust the **step** value to control how much the counter increases or decreases.
+
+- ➕➖ Increment or decrement the **count** by the current step.
+
+- 📅 Dynamically calculates and displays the new date based on the current count.
+
+- 🗓️ Smart messages like “3 days ago” or “5 days from today”.
+
+## 🛠 Tech Stack
+
+⚛️ **React** (with Hooks)
+
+📦 **JavaScript** (ES6+)
+
+🎨 **HTML & CSS**
---
+++ Apps/darkMode/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="card">
+      <div className="rows">
+        <button onClick={() => setStep(s => s - 1)}>-</button>
+        <h3>Step:{step} </h3>
+        <button onClick={() => setStep(s => s + 1)}>+</button>
+      </div>
+      <div className="rows">
+        <button onClick={() => setCount(c => c - step)}>-</button>
+        <h3>Count:{count} </h3>
+        <button onClick={() => setCount(c => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} Days from today is `
+            : `${Math.abs(count)} Days ago was `}
+        </span>
+
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
